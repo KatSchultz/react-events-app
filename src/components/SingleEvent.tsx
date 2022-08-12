562be0d03807f629b31338cf47f7f511f856032a
KatSchultz <ks.katieschultz@gmail.com>
2022-08-11 21:27:46 -0400

    styled single events

--- src/components/SingleEvent.tsx
+++ src/components/SingleEvent.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import "./SingleEvent.css";
 
-export default function SingleEvent() {
-  return <div>SingleEvent</div>;
+interface Props {
+  event: {
+    name: string;
+    type: string;
+    id: string;
+    test: boolean;
+    url: string;
+  };
+}
+
+export default function SingleEvent({ event }: Props) {
+  function bucketClickHandler() {
+    // setBucketList(...BucketList, event)
+  }
+
+  return (
+    <div className="single-event">
+      <div className="date">
+        <div>date</div>
+        <div>day</div>
+        <div>time</div>
+      </div>
+      <div className="event-details">
+        <div className="event-name">{event.name}</div>
+        <div>Location</div>
+      </div>
+      <div className="button" onClick={bucketClickHandler}>
+        <div>
+          Add to <br />
+          Bucket List
+        </div>
+      </div>
+    </div>
+  );
 }
